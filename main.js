f7b1a5ad4d0b9c7e7e8290c5236b853af2227b79
Zach Roark <zach.roark@yahoo.com>
2020-04-28 14:49:19 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+
+// Not sure why it continues to return undefined
+
+function DG(dog) {
+    if (dog === undefined) {
+        // return `I like cats!`
+      console.log(`I like cats!`)
+    } else {
+        // return `my favorite dog breed is ${dog}`
+        console.log(`my favorite dog breed is ${dog}`)
+    }}
+DG("pug");
+// console.log("when it comes to pets, " + DG())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Functions can return a value that we can capture
+
+
+// Write a function that takes a string of a dog breed as 
+// an argument (like 'border collie')
+
+// Have the function return "my favorite dog breed is" plus 
+// the passed in string. If no argument is passed to the function, 
+// return "I like cats".
+
+// Execute the function in a way that captures the returned value 
+// in a variable.
+
+// Console.log the string "When it comes to pets," plus the 
+// returned value of the function.
